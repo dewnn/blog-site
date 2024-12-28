cf652f863d98985a12f1b784a5ece46f1c713eee
dewn <dewn@example.com>
2024-12-28 23:09:50 +0300

    fix: remove unused node parameter

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -36,7 +36,7 @@ export default async function PostPage({ params }: Props) {
         <ReactMarkdown
           className="text-gray-800 dark:text-gray-200"
           components={{
-            code({node, inline, className, children, ...props}) {
+            code({inline, className, children, ...props}) {
               const match = /language-(\w+)/.exec(className || '');
               return !inline && match ? (
                 <SyntaxHighlighter
